f1c882f647ddebc5614ffa1d504b17e10f1a763b
Satrio Arditama <iyo@amilna.com>
2015-02-05 15:25:57 +0700

    index

--- views/default/index.php
+++ views/default/index.php
@@ -12,7 +12,7 @@ use yii\helpers\Url;
 
         <p class="lead">You have successfully installed Company Accounting Plugin for your Yii-powered application.</p>
 
-        <p><?= Html::a(Yii::t('app','Get start to create an account'),'account/create',["class"=>"btn btn-lg btn-success"])?></p>
+        <p><?= Html::a(Yii::t('app','Get start to create an account'),['//cap/account/create'],["class"=>"btn btn-lg btn-success"])?></p>
     </div>
 
     <div class="body-content">
@@ -23,21 +23,21 @@ use yii\helpers\Url;
 
                 <p>Formal record that represents, in words, money or other unit of measurement, certain resources, claims to such resources, transactions or other events that result in changes to those resources and claims.</p>
 
-                <p><?= Html::a(Yii::t('app','Go to Account'),'account',["class"=>"btn btn-primary"])?></p>
+                <p><?= Html::a(Yii::t('app','Go to Account'),['//cap/account'],["class"=>"btn btn-primary"])?></p>
             </div>
             <div class="col-lg-4">
                 <h2>Transaction</h2>
 
                 <p>The act of transacting, especially a business agreement or exchange; event or condition recognized by an entry in the book ACCOUNT.</p>
 
-                <p><?= Html::a(Yii::t('app','Go to Transaction'),'transaction',["class"=>"btn btn-primary"])?></p>
+                <p><?= Html::a(Yii::t('app','Go to Transaction'),['//cap/transaction'],["class"=>"btn btn-primary"])?></p>
             </div>
             <div class="col-lg-4">
                 <h2>Journal</h2>
 
                 <p>Any book containing original entries of daily financial transactions.</p>
 
-                <p><?= Html::a(Yii::t('app','Go to Journal'),'journal',["class"=>"btn btn-primary"])?></p>
+                <p><?= Html::a(Yii::t('app','Go to Journal'),['//cap/journal'],["class"=>"btn btn-primary"])?></p>
             </div>
         </div>
 
