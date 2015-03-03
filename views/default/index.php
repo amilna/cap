c7e0ce4bcf0b7c5a182adcbf8c8687bed981ef31
Satrio Arditama <iyo@amilna.com>
2015-03-03 13:57:09 +0700

    update

--- views/default/index.php
+++ views/default/index.php
@@ -18,21 +18,21 @@ use yii\helpers\Url;
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <h2>Account</h2>
 
                 <p>Formal record that represents, in words, money or other unit of measurement, certain resources, claims to such resources, transactions or other events that result in changes to those resources and claims.</p>
 
                 <p><?= Html::a(Yii::t('app','Go to Account'),['//cap/account'],["class"=>"btn btn-primary"])?></p>
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <h2>Transaction</h2>
 
                 <p>The act of transacting, especially a business agreement or exchange; event or condition recognized by an entry in the book ACCOUNT.</p>
 
                 <p><?= Html::a(Yii::t('app','Go to Transaction'),['//cap/transaction'],["class"=>"btn btn-primary"])?></p>
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <h2>Journal</h2>
 
                 <p>Any book containing original entries of daily financial transactions.</p>
